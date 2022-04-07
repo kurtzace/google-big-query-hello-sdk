66f0a5bedd5661a8031d21416ec784251a1c10db
Karan Bhandari <kurtzace@aol.com>
2022-04-07 12:14:26 +0530

    user inputs or args

--- BigQueryPublicTableForIds/Program.cs
+++ BigQueryPublicTableForIds/Program.cs
@@ -12,26 +12,61 @@ namespace BigQueryPublicTableForIds
             StartupUtility.LoadSettings();
             Console.WriteLine("Enter choice:\r\n1.ViewRecords (arg tableName).\r\n2.InsertRecords (args fileName.csv)");
             var bqIntegrator = new GoogleBigQueryIntegrator();
+
+            if (args.Length == 0)
+            {
+                UserInput(bqIntegrator);
+                Finish();
+                Console.ReadLine();
+                return;
+            }
+
+            if (args[0]=="1")
+            {
+                View(bqIntegrator);
+            } else
+            {
+                Write(bqIntegrator, args[1]);
+            }
+            Finish();
+
+
+        }
+
+        private static void Finish()
+        {
+            Console.WriteLine("----------");
+        }
+
+        private static void UserInput(GoogleBigQueryIntegrator bqIntegrator)
+        {
             var str = Console.ReadLine();
             switch (str)
             {
                 case "1":
                 case "ViewRecords":
-                    Console.WriteLine(string.Join(Environment.NewLine, bqIntegrator.ReadTopTenRows("bigquery-public-data.sdoh_cdc_wonder_natality.county_natality_by_maternal_morbidity")["Births"]));
+                    View(bqIntegrator);
                     break;
                 case "2":
                 case "InsertRecords":
                     Console.WriteLine("fileName.csv:");
                     var fileName = Console.ReadLine();
-                    var lines = System.IO.File.ReadAllLines(fileName).Skip(1);
-                    bqIntegrator.CreateTable(lines, $"articleids_{DateTime.Now:MMdd}", "an");
+                    Write(bqIntegrator, fileName);
                     break;
                 default:
                     break;
             }
-            Console.WriteLine("----------");
-            Console.ReadLine();
+        }
 
+        private static void Write(GoogleBigQueryIntegrator bqIntegrator, string fileName)
+        {
+            var lines = System.IO.File.ReadAllLines(fileName).Skip(1);
+            bqIntegrator.CreateTable(lines, $"articleids_{DateTime.Now:MMdd}", "an");
+        }
+
+        private static void View(GoogleBigQueryIntegrator bqIntegrator)
+        {
+            Console.WriteLine(string.Join(Environment.NewLine, bqIntegrator.ReadTopTenRows("bigquery-public-data.sdoh_cdc_wonder_natality.county_natality_by_maternal_morbidity")["Births"]));
         }
     }
 }
